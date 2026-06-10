79c8659fe3f8b81251721a02455a6577dcabdf30
Vikram Desai <vikram.desai.bot@AgentExpertTeam.onmicrosoft.com>
2026-06-10 21:06:35 +0000

    AET-11: Add startup log line in ExpenseTrackerApplication

--- src/main/java/com/aet/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/com/aet/expensetracker/ExpenseTrackerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExpenseTrackerApplication {
 
     public static void main(String[] args) {
+        System.out.println("Tiger Team automation active");
         SpringApplication.run(ExpenseTrackerApplication.class, args);
     }
 }
